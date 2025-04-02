c93d495b468aee155af7065984ccfbb46e5853b2
abhishek92132 <133634413+abhishek92132@users.noreply.github.com>
2025-04-02 10:11:10 +0530

    page correction

--- src/app/media/videos/page.jsx
+++ src/app/media/videos/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import VideoCarousel from './Video'
+import VideoCarousel1 from './Video1'
 
 const page = () => {
   return (
     <div>
         <VideoCarousel />
+        <VideoCarousel1 />
     </div>
   )
 }
